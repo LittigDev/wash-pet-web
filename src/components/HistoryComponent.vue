628385b3250e49a1ee2089c548cf229594d28666
R-Eduardo <raimundo_eduardo@outlook.com>
2023-05-14 19:33:59 -0300

    History_ajust

--- src/components/HistoryComponent.vue
+++ src/components/HistoryComponent.vue
@@ -1,14 +1,15 @@
 <template>
 	<div class="w-card-container">
-		<v-card
-			class="w-card-content"
-			v-for="item in service"
-			:key="item.typeService"
-		>
-			<p>{{ item.name }}</p>
-			<p>{{  item.typeService}}</p>
-			<p>{{ item.date }}</p>
-			<p>{{  item.value}}</p>
+		<v-card class="w-card-content" v-for="item in service" :key="item.typeService" >
+			<div style="display: flex; margin-top: 15px ; margin-left:15px; margin-bottom: 15px;  width: 10px; text-align:left;  justify-content: space-between; align-items: flex-start ">
+				<img src="../assets/asseio.png" width="45" height="45"/>
+				<div style="margin-left: 15px; text-align: left;">
+					<p style="font-size: 15px;">{{ item.name }}</p>
+					<p style="font-size: 20px;">{{  item.typeService}}</p>
+					<p style="font-size: 11px;">{{ item.date }}</p>
+					<p style="font-size: 20px;">{{  item.value}}</p>
+				</div>	
+			</div>	
 		</v-card>
 	</div>
 </template>
@@ -19,9 +20,9 @@
 		data() {
 			return {
 				service: [
-					{ name: 'Inf. Pessoais', typeService: 'PerfilComponent' , date: '12/12/2020',value: 'R$4,99'},
-					{ name: 'Histórico', typeService: 'HistoryComponent' , date: '12/12/2020',value: 'R$4,99'},
-					{ name: 'Carteira', typeService: 'WalletComponent', date: '12/12/2020',value: 'R$4,99' }
+					{ name: 'Serviço', typeService: 'Banho e Tosa' , date: '12/12/2020',value: 'R$80,00'},
+					{ name: 'Serviço', typeService: 'Banho e Tosa' , date: '05/11/2020',value: 'R$80,00'},
+					{ name: 'Serviço', typeService: 'Aparar Garras', date: '01/01/2020',value: 'R$35,00'}
 				]
 			}
 		}
@@ -31,15 +32,22 @@
 
 <style lang="scss">
 .w-card-container {
+	
+	background-color: #E0F3F0;
 	display: flex;
+	
 	flex-direction: column;
-	justify-content: center;
-	width: 100%;
+	//justify-content:baseline ;
+	//width: 100%;
+	padding: 10%;
 
 	.w-card-content {
 		margin: 10px;
-		max-width: 80%;
+		max-width: auto;
+		
 	}
 }
+
+
 
 </style>
